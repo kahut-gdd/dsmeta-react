68eb69050a12d4cb10612764c59821f3f531a719
kahut.dev <dagmar.kahutpp@gmail.com>
2022-09-07 21:03:34 -0300

    API test

---
+++ backend/src/main/java/com/kahut/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.kahut.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kahut.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
